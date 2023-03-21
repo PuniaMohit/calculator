f18f15f55473b4af380d7292869e85590760571b
Mohit Punia <97046203+MOHIT-0001@users.noreply.github.com>
2023-03-21 11:10:36 +0530

    updated

--- calculator.js
+++ calculator.js
@@ -1,16 +1,49 @@
-function dis(val)
-{
-document.getElementById("edu").value+=val
+
+
+
+function dis(a){	
+    document.getElementById("ans").value+=a
 }
 
-function solve()
-{
-let x = document.getElementById("edu").value
-let y = eval(x)
-document.getElementById("edu").value = y
+function che(){
+    var ans=document.getElementById("ans").value
+    console.log(ans)
+		var res = [];
+		var answer;
+		
+		if(ans.includes("+")){
+            console.log(ans)
+		res = ans.split('+');
+        console.log(res)
+		return parseInt(res[0]) + parseInt(res[1]);
+		}
+		
+		else if(ans.includes("-")){
+		res = ans.split('-');
+		return parseInt(res[0]) - parseInt(res[1]);
+		}
+		
+		else if(ans.includes("/")){
+		res = ans.split('/');
+		return parseInt(res[0]) / parseInt(res[1]);
+		}
+		
+		else{
+		res = ans.split('*');
+		return answer= parseInt(res[0]) * parseInt(res[1]);
+		}
 }
+		
+function solve(){	
+var a = che()
+console.log(a)
+document.getElementById("ans").value = a; 
+}
+	
+function clr(){
+       document.getElementById("ans").value="";
+			
+	}
 
-function clr()
-{
-document.getElementById("edu").value = ""
-}
+
+
